b26caa01a764b079764af03d255ce901b711a4b4
benjamin <benjaminzhiyuan.lee@digipen.edu>
2023-09-24 02:04:48 +0800

    PA 3 complete

--- PA3 - 3D Arrays/tddaa.cpp
+++ PA3 - 3D Arrays/tddaa.cpp
@@ -1,49 +1,69 @@
-/*
-Read the following to figure out what functions allocate and deallocate must do:
-
-Given three constant integer values F, R, and C, the definition
-  int a[F][R][C]; 
-specifies a static "array of arrays of arrays" [or, a three-dimensional array] where,
-F specifies the number of frames,
-R specifies the number of rows in each frame, and
-C specifies the number of columns within each row.
-
-A specific element in the 3D array a is accessed through expression: a[i][j][k] where
-i chooses a specific frame [out of a total of F]
-j chooses a specific row within the frame [out of a total of R rows per frame], and
-k chooses a specific element within the row [out of a total of C columns per row].
-
-You must define functions allocate and deallocate with the following specifications.
-
-Function allocate takes three int parameters for simulating a 3D array:
-the number of frames (F), number of rows within each frame (R), and
-the number of columns (C) within each row. You must do the following:
-a) You must allocate a block of heap memory for storing all the int elements in the
-simulated 3D array.
-b) Next, you must allocate a block of heap memory that will store pointers to each row
-in the 3D array [simulated by the previously allocated block of heap memory]. It is
-your responsibility to assign to each element of this array [of pointers] the address
-of each row in the heap memory allocated in a).
-c) Finally, you must allocate a block of heap memory that will store pointers to each
-of the F frames. It is your responsibility to assign to each element of this array
-[of pointers] the address of each frame in the heap memory allocated in b).
-d) Function allocate will return the address of the starting byte of the block of
-heap memory allocated in c).
-
-Function deallocate will take the address returned by function allocate and return
-to the free store the blocks of memory dynamically allocated in function allocate
-
-Your code must be implemented using C++ memory allocation/deallocation operators.
-Obviously, your submission should not have memory leaks or other memory related bugs.
-*/
+/*!******************************************************************
+ * \file      tddaa.cpp
+ * \author    Benjamin Lee
+ * \par       DP email: benjaminzhiyuan.lee\@digipen.edu.sg
+ * \par       Course: CSD2126
+ * \par       Section: B
+ * \par      
+ * \date      24-09-2023
+ * 
+ * \brief     Function definition for allocate and deallocate.
+ *********************************************************************/
 
 #include "tddaa.h"
 
-int*** allocate(int F, int R, int C) {
-  // complete definition of function allocate ...
-  return nullptr;
+/**
+ * @brief Allocates memory for a three-dimensional array of integers.
+ *
+ * This function allocates memory for a three-dimensional array of integers
+ * with the specified number of frames, rows, and columns.
+ *
+ * @param F The number of frames.
+ * @param R The number of rows per frame.
+ * @param C The number of columns per row.
+ *
+ * @return A pointer to the allocated three-dimensional array.
+ */
+int ***allocate(int F, int R, int C)
+{
+  // Allocate memory for the element array
+  int *element = new int[F * R * C];
+
+  // Allocate memory for the row pointers
+  int **row = new int *[F * R];
+
+  // Allocate memory for the frame pointers
+  int ***frame = new int **[F];
+
+  // Initialize a two-dimensional array of pointers, representing frames, rows, and elements
+  for (int i = 0; i < F; i++) // Loop through frames (F represents the number of frames)
+  {
+    for (int j = 0; j < R; j++) // Loop through rows (R represents the number of rows per frame)
+    {
+      // Calculate the index for the current element in the 2D array
+      // and assign it to the 'row' array as a pointer to the element.
+      // 'element' is the base address, and (i * R + j) represents the offset.
+      row[i * R + j] = element + (i * R + j) * C;
+    }
+    // Assign the 'row' array as a pointer to the current frame in the 'frame' array.
+    frame[i] = row + i * R;
+  }
+
+  // Return the 'frame' array containing pointers to the 2D array of elements.
+  return frame;
 }
 
-void deallocate( int *** ppp ) {
-  // complete definition of function deallocate ...
+/**
+ * @brief Deallocates memory associated with a three-dimensional array of integers.
+ *
+ * This function deallocates memory that was previously allocated for a
+ * three-dimensional array of integers using the `allocate` function.
+ *
+ * @param ppp A pointer to the three-dimensional array to be deallocated.
+ */
+void deallocate(int ***ppp)
+{
+  delete[] ppp[0][0];
+  delete[] ppp[0];
+  delete[] ppp;
 }
