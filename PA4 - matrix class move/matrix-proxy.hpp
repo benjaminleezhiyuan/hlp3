b9d9bd8ac44e3ae42804871c999db6eecceb5796
benjamin <benjaminzhiyuan.lee@digipen.edu>
2023-10-01 01:22:57 +0800

    added assignment 4

---
+++ PA4 - matrix class move/matrix-proxy.hpp
@@ -0,0 +1,31 @@
+// Don't forget file-header and function-level Doxygen documentation blocks!!!
+
+// First, define the class template. Then, define the member functions of the class template
+// OUTSIDE the class definition. The automatic grader will not be able to check for this - 
+// however, the manual grader can. Occurrences of EACH such function defined in the class definition
+//  will result in a deduction of ONE LETTER GRADE!!! You're forewarned!!!
+
+// This is how you should implement the assignment:
+
+#ifndef MATRIX_PROXY_HPP
+#define MATRIX_PROXY_HPP
+
+// include necessary headers ...
+
+namespace HLP3 {
+
+/*
+
+define class template Matrix ...
+
+declare global functions ...
+
+define class template Matrix's member functions here ...
+
+define global functions ...
+
+*/
+
+}
+
+#endif
