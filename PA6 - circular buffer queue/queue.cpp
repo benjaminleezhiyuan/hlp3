8ce22178676a3208ab83d91710a98237ac39261f
benjamin <benjaminzhiyuan.lee@digipen.edu>
2023-11-09 16:09:39 +0800

    tail issue now

--- PA6 - circular buffer queue/queue.cpp
+++ PA6 - circular buffer queue/queue.cpp
@@ -1,12 +1,164 @@
 #include "queue.hpp"
-// don't include any other headers files [otherwise, the online grader will
-// not accept your submission ...]
 
-namespace HLP3 {
+namespace HLP3
+{
+    // Constructor with capacity
+    Queue::Queue(size_type _capacity) : head(0), tail(0), _capacity(_capacity), current_size(0)
+    {
+        buffer = new value_type[_capacity];
+    }
 
-// provide implementation using the exact explanation of circular buffers
-// that was provided in specs
+    // Copy constructor
+    Queue::Queue(const Queue &other) : head(other.head), tail(other.tail), _capacity(other._capacity), current_size(other.current_size)
+    {
+        buffer = new value_type[_capacity];
+        for (size_type i = 0; i < current_size; ++i)
+        {
+            buffer[i] = other.buffer[i];
+        }
+    }
 
-// for more details, see the interface used by the driver ...
+    // Copy assignment operator
+    Queue &Queue::operator=(const Queue &other)
+    {
+        // Avoid self-assignment
+        delete[] buffer; // Free the existing memory
 
+        head = other.head;
+        tail = other.tail;
+        _capacity = other._capacity;
+        current_size = other.current_size;
+
+        buffer = new value_type[_capacity]; // Allocate new memory
+
+        // Copy elements
+        for (size_type i = 0; i < _capacity; ++i)
+        {
+            buffer[i] = other.buffer[i];
+        }
+
+        return *this;
+    }
+
+    // Destructor
+    Queue::~Queue()
+    {
+        delete[] buffer;
+    }
+
+    // Definition of c_buff member function
+    Queue::const_pointer Queue::c_buff() const
+    {
+        return buffer;
+    }
+
+    bool Queue::empty() const
+    {
+        return current_size == 0;
+    }
+
+    bool Queue::full() const
+    {
+        return current_size == _capacity;
+    }
+
+    Queue::size_type Queue::size() const
+    {
+        return current_size;
+    }
+
+    Queue::size_type Queue::capacity() const
+    {
+        return _capacity;
+    }
+
+    void Queue::push(const value_type &element)
+    {
+        if (full())
+        {
+            head = (head + 1) % _capacity; // Increment head and when it reaches N set to 0 (% -capacity)
+        }
+
+        else
+        {
+            // If not full, increment size
+            ++current_size;
+        }
+
+        // Add the new element at the tail
+        back() = element;
+    }
+
+    void Queue::pop()
+    {
+        if (!empty())
+        {
+            head = (head + 1) % _capacity; // Increment head
+            --current_size;                // Decrement size
+        }
+    }
+
+    Queue::reference Queue::front()
+    {
+
+        return buffer[head];
+    }
+
+    Queue::const_reference Queue::front() const
+    {
+
+        return buffer[head];
+    }
+
+    Queue::reference Queue::back()
+    {
+
+        return buffer[((head + current_size - 1) % _capacity)];
+    }
+
+    Queue::const_reference Queue::back() const
+    {
+
+        return buffer[((head + current_size - 1) % _capacity)];
+    }
+
+    Queue::size_type Queue::front_index() const
+    {
+        return head;
+    }
+
+    Queue::size_type Queue::back_index() const
+    {
+        return (head + current_size) % _capacity;
+    }
+
+    void Queue::swap(Queue &other)
+    {
+        // Swap the individual data members
+        size_type temp_head = head;
+        head = other.head;
+        other.head = temp_head;
+
+        size_type temp_tail = tail;
+        tail = other.tail;
+        other.tail = temp_tail;
+
+        size_type temp_capacity = _capacity;
+        _capacity = other._capacity;
+        other._capacity = temp_capacity;
+
+        size_type temp_size = current_size;
+        current_size = other.current_size;
+        other.current_size = temp_size;
+
+        // Swap the buffer
+        value_type *temp_buffer = buffer;
+        buffer = other.buffer;
+        other.buffer = temp_buffer;
+    }
+
+    void swap(Queue &first, Queue &second)
+    {
+        first.Queue::swap(second);
+    }
 }
--- PA6 - circular buffer queue/queue.hpp
+++ PA6 - circular buffer queue/queue.hpp
@@ -1,28 +1,81 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
-namespace HLP3 {
+namespace HLP3
+{
 
-class Queue {
-public:
-  using value_type = char;
-  using reference  = value_type&;
-  using const_reference = const value_type&;
-  using pointer    = value_type*;
-  using const_pointer = const value_type*;
-  using size_type = unsigned long;
+  class Queue
+  {
+  public:
+    using value_type = char;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using size_type = unsigned long;
 
-// provide rest of interface ...
-// don't declare any data members or anything else to be of type short or int ...
+    // No default constructor
+    Queue() = delete;
 
-// if size_type is changed from unsigned long to unsigned int, your implementation
-// should work perfectly without any other edits to queue.hpp and queue.cpp ...
+    // Constructor with capacity
+    Queue(size_type capacity);
 
-// if value_type is changed from char to any other type, your implementation
-// should work perfectly without any other edits to queue.hpp and queue.cpp ...
-};
+    // Copy constructor
+    Queue(const Queue &other);
 
-// declare necessary non-member function(s) ...
+    // Destructor
+    ~Queue();
+
+    // Copy assignment operator
+    Queue &operator=(const Queue &other);
+
+    // Return pointer to queue buffer
+    const_pointer c_buff() const;
+
+    bool empty() const;         // Check if the queue is empty
+    bool full() const;          // Check if the queue is full
+    size_type size() const;     // Return the current size of the queue
+    size_type capacity() const; // Return the capacity of the queue
+
+    // Add new element to the buffer
+    void push(const value_type &element);
+
+    // Rmove oldest element in buffer
+    void pop();
+
+    // Accessor and mutator for oldest element
+    reference front();
+
+    // Accessor only for oldest element
+    const_reference front() const;
+
+    // Accessor and mutator for newest element
+    reference back();
+
+    // Accesor only for newest element
+    const_reference back() const;
+
+    // Return indices to oldest element
+    size_type front_index() const;
+
+    // Return indices to newest element
+    size_type back_index() const;
+
+    // Swap current object of type Queue with another object of type Queue
+    void swap(Queue &other);
+
+  private:
+    size_type head;
+    size_type tail;
+    size_type _capacity;
+    size_type current_size;
+    pointer buffer;
+  };
+
+  // declare necessary non-member function(s) ...
+
+  // Swap two objects of type Queue
+  void swap(Queue &first, Queue &second);
 
 }
 #endif
